473f05d663209d14434fcdd3fe55ea07a7af9c2b
moe18 <chabotmoe1@gmail.com>
2024-01-04 17:28:22 -0500

    finished up the PCA example

--- example_PCA.py
+++ example_PCA.py
@@ -2,21 +2,19 @@ import d0mbpy.d0mbpy as dp
 import numpy as np
 from numpy.linalg import eig
 
-a = np.random.rand(3,5)
-#a = np.array([[1,2,3],[4,5,6],[7,8,9],[1,2,3]])
+a = np.random.rand(3,3)
 x = dp.LinAlg(a)
-x = dp.LinAlg([[1,1,0],
-            [1,0,1],
-            [0,1,1]])
-#x = dp.LinAlg(a)
 
 
 # normilize data
 x_norm  = (x - x.mean(axis=0)) / x.std(axis=0)
 
+# get cov of data
 x_cov = x_norm.cov()
-
-x_cov = dp.LinAlg([[2.92,.86,-1.15],[.86,6.51,3.32],[-1.15,3.32,4.57]])
+# get eigen values and eigen vectors
 e_vals, e_vec = x_cov.eigen_values()
-print(e_vals, e_vec.transpose())
 
+n = 2
+pca = (dp.LinAlg(e_vec.transpose()[:n])* x_norm.transpose()).transpose()
+
+print(pca)
