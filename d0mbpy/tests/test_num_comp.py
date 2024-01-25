3378ce47f9833b52b0f863c67dc0cfcce7ea3be7
moe18 <chabotmoe1@gmail.com>
2024-01-25 12:52:41 -0500

    added class for backprop

--- d0mbpy/tests/test_num_comp.py
+++ d0mbpy/tests/test_num_comp.py
@@ -9,14 +9,22 @@ def assert_matrices_equal(mat1, mat2, tol=1e-6):
             assert abs(mat1.data[i][j] - mat2.data[i][j]) < tol
 
 
-def assert_equation(val, val1, tol=1e-6):
+def assert_equation(val, val1, tol=1e-4):
     assert abs(val - val1)< tol
 
 
-def test_matrix_addition():
+def test_softmax():
     a = NumComp()
     val = a.softmax(2,LinAlg([[1,2,3]]))
 
     expected_result = .244728
     print(val)
     assert_equation(val, expected_result)
+
+def test_dir():
+    def f(x):
+        return x**2
+    
+    a = NumComp(f)
+
+    assert_equation(a.dirivative(), 6)
