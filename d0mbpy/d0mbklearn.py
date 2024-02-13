dd5704ad5b041b69df8b8a118a2fcf8220807a67
moe18 <chabotmoe1@gmail.com>
2024-02-12 21:39:59 -0500

    finished k folds

--- d0mbpy/d0mbklearn.py
+++ d0mbpy/d0mbklearn.py
@@ -13,48 +13,72 @@ class lin_equation:
     def predict(self,x):
         return x.T * self.w
     
+    
+class Loss:
+
+    def __init__(self,y_hat= 0,y= 0):
+        self.y = y
+        self.y_hat = y_hat
+
+
     @staticmethod
     def mse(y_hat,y):
         return (1/len(y))*(y_hat - y).T@(y_hat - y)
 
-
 class kfolds:
-    def __init__(self, data, model, loss, k_fold):
-        self.data = data
+    def __init__(self, x, y, model, loss, k_fold):
+        self.x = x
+        self.y = y
         self.model = model
         self.loss = loss
         self.k_fold = k_fold
         
-
-    def split(self):
-        splits = len(self.data)//self.k_fold
+    @staticmethod
+    def split(data,k_fold):
+        splits = len(data)//k_fold
         split_data = []
-        for i in range(0,len(self.data),splits):
-            split_data.append(self.data[i:i+splits])
+        for i in range(0,len(data),splits):
+            split_data.append(data[i:i+splits])
         return split_data
     
 
-    # need to create a way to pass x and y into spliter and fit the model correctly
     def run(self):
-        folds = self.split()
+        x = self.split(self.x, self.k_fold)
+        y = self.split(self.y, self.k_fold)
+
+        x_copy = x.copy()
+        y_copy = y.copy()
+
+        loss = []
 
         for i in range(self.k_fold):
-            self.model.fit()
-            for j in 
+            val_x = x_copy.pop(i)
+            val_y = y_copy.pop(i)
+
+            x_train = np.array(x_copy).flatten()
+            y_train = np.array(y_copy).flatten()
+
+            self.model.fit(x_train,y_train)
+            pred = np.array(self.model.predict(np.array(val_x)))
+
+            val_y = np.array(val_y)
+            
+            loss.append(self.loss.mse(pred, val_y))
+
+            x_copy = x.copy()
+            y_copy = y.copy()
+
+            self.model.w = None
+        return loss
+
+
+
 
         
 
-
 x = np.array([1,2,3,4,5,6,7,8,9,10])
 
-split(x,5)
-y = np.array([2,4,5,7,8])
 
-
-model   = lin_equation()
-model.fit(x,y)
-
-pred = model.predict(x)
-
-print(model.mse(pred,y))
+y = np.array([2,4,6,8,10,1,3,5,7,9])
+print(kfolds(x,y,lin_equation(),Loss,5).run())
 
