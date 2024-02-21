509851d16e86cc7d257dbf78e5c90dffbfa510ba
moe18 <chabotmoe1@gmail.com>
2024-02-21 18:57:37 -0500

    finished SVM

--- d0mbpy/d0mbklearn.py
+++ d0mbpy/d0mbklearn.py
@@ -1,3 +1,4 @@
+from typing import Any
 import numpy as np
 import random
 
@@ -28,6 +29,9 @@ class Loss:
     @staticmethod
     def cross_ent(pred,prob):
         return -np.mean(np.sum(np.log(pred+.00001)*prob))
+
+
+
 
 
 class bayesLinReg:
@@ -66,8 +70,18 @@ class bayesLinReg:
     def pred_var(self,x):
         return x**2 * self.w_var**2 + self.y_var**2
     
+# need to work on this
+class bayesClassification(bayesLinReg):
+    def __init__(self, x, y) -> None:
+        super().__init__(x, y)
+    
 
+    @staticmethod
+    def sigmoid(x):
+        return(np.exp(x))/(1+np.exp(x))
 
+    def predict(self, x):
+        return self.sigmoid(super().predict(x))
 
 class kfolds:
     def __init__(self, x, y, model, loss, k_fold):
@@ -117,5 +131,76 @@ class kfolds:
 
 
 
-#print(kfolds(x,y,lin_equation(),Loss.mse,5).run())
 
+class SVM:
+    def __init__(self, lr=.1, iterations=1000) -> None:
+        self.iterations = iterations
+        self.lr=lr
+        self.w = None
+        self.b = None
+
+    def fit(self, x,y):
+
+        #x = (x * x +1)**2
+        
+        
+        w = np.random.randn(x.shape[1])
+        b = np.random.randn(x.shape[1])
+
+        for i in range(self.iterations):
+            pred = x @ w.T + b
+
+
+
+            loss = self.hinge_loss(x,y,w,b)
+            w_grad = np.sum(self.gradient_w(x,y,w,pred))
+            b_grad = np.sum(self.gradient_b(x,y,pred))
+            w = w - self.lr * w_grad
+            b = b - self.lr * b_grad
+
+            pred = x @ w.T + b
+
+            loss = self.hinge_loss(x,y,w,b)
+            print(loss)
+        self.w = w
+        self.b = b
+    
+    def predict(self,x):
+        #x = (x * x +1)**2
+        return x @ self.w.T + self.b
+
+    @staticmethod
+    def gradient_w(x,y,w,pred,lama=.1):
+        w_grad = np.where(1-y*(pred) >0, lama*w - 1/len(x) * np.sum(y*x),0)
+        return w_grad
+    
+    @staticmethod
+    def gradient_b(x,y,pred):
+        b_grad = np.where(1-y*(pred) >0, 1/len(x) * np.sum(y),0)
+        return b_grad
+
+    @staticmethod
+    def hinge_loss(x,y,w,b, lama=.1):
+        y_pred = x @ w.T + b
+        return 1/len(x) * np.sum(np.where(y-y_pred>0,y-y_pred,0) + lama * np.linalg.norm(w))
+
+
+class1_x = np.random.randn(20, 1) + np.array([2])  # Shift class 1 points
+
+# Generate points for Class 2
+#class2_x = np.random.randn(10, 2) + np.array([-2, -2])  # Shift class 2 points
+
+# Labels
+class1_y = np.ones(10)  # Class 1 labels
+class2_y = -np.ones(10)  # Class 2 labels
+
+# Combine into one dataset
+X = class1_x#np.vstack((class1_x, class2_x))
+y = np.concatenate((class1_y, class2_y))
+
+model = SVM()
+model.fit(X,y)
+
+print('x',X[2])
+print('y',y[2])
+print(model.predict(X[2])[0])
